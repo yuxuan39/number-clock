ae824fdf684a5894c98dcc57d8380a033783f05e
yuxuan39 <kumo9102151@gmail.com>
2017-09-09 23:00:25 +0800

    add 0

--- src/index.js
+++ src/index.js
@@ -2,17 +2,28 @@ import './styles/style.scss'
 
 const updateTime = () => {
     const date = new Date()
-    const hr = date.getHours()
-    const min = date.getMinutes()
-    const sec = date.getSeconds()
+    let hr = date.getHours()
+    let min = date.getMinutes()
+    let sec = date.getSeconds()
     const year = date.getFullYear()
     const month = date.getMonth()
     const day = date.getDay
+    
+    if (hr < 10) {
+        hr = '0'+ hr
+    }
+    if (min < 10) {
+        min = '0' + min
+    }
+    if (sec < 10) {
+        sec = '0' + sec
+    }
+    
     const time = document.getElementById('time')
     const newTime = hr + ':' + min + ':' + sec
-
     time.innerHTML = newTime
     setTimeout(updateTime, 500);
 }
+
 
 updateTime()
