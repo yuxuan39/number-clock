63b022273c5588e245fb77b98a932f271e0a8b0a
yuxuan39 <kumo9102151@gmail.com>
2017-09-09 19:06:45 +0800

    clock is working

--- src/index.js
+++ src/index.js
@@ -1,2 +1,18 @@
 import './styles/style.scss'
 
+const updateTime = () => {
+    const date = new Date()
+    const hr = date.getHours()
+    const min = date.getMinutes()
+    const sec = date.getSeconds()
+    const year = date.getFullYear()
+    const month = date.getMonth()
+    const day = date.getDay
+    const time = document.getElementById('time')
+    const newTime = hr + ':' + min + ':' + sec
+
+    time.innerHTML = newTime
+    setTimeout(updateTime, 500);
+}
+
+updateTime()
