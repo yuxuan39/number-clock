385b8763ed2c5cecdab49d22c7bd3afd10696749
yuxuan39 <kumo9102151@gmail.com>
2017-09-10 16:51:12 +0800

    add date and day

--- src/index.js
+++ src/index.js
@@ -5,9 +5,6 @@ const updateTime = () => {
     let hr = date.getHours()
     let min = date.getMinutes()
     let sec = date.getSeconds()
-    const year = date.getFullYear()
-    const month = date.getMonth()
-    const day = date.getDay
     
     if (hr < 10) {
         hr = '0'+ hr
@@ -20,10 +17,38 @@ const updateTime = () => {
     }
     
     const time = document.getElementById('time')
+    
     const newTime = hr + ':' + min + ':' + sec
     time.innerHTML = newTime
-    setTimeout(updateTime, 500);
+    setTimeout(updateTime, 500)
+
+}
+
+const updateDate = () => {
+    const date = new Date()
+    const year = date.getFullYear()
+    const month = date.getMonth()
+    const dt = date.getDate()
+    const day = date.getDay()
+    const today = document.getElementById('today')
+    const whatDay = document.getElementById('whatday')
+    const newToday = year + '/' + month + '/' + dt
+    const array = ['日','一','二','三','四','五','六']
+
+    today.innerHTML = newToday
+    whatDay.innerHTML = '星期'+ array[day]
 }
 
 
+let x = 0
+
+const movingBg = () => {
+    x = x + 0.2
+    let bgPosition = document.getElementById('body').style.backgroundPosition = x +'%' + '0%'
+    // console.log(bgPosition)
+    setTimeout(movingBg, 30)
+}
+
 updateTime()
+updateDate()
+movingBg()
